900885abeb4a7a3a4d12f766b5c06afccaf2c3c3
hanrattyjen <hanratty.jen@gmail.com>
2016-09-16 11:50:24 +0100

    Added interactive menu.

--- directory.rb
+++ directory.rb
@@ -9,6 +9,14 @@ def input_students
   while !name.empty? do
     puts "Cohort month:"
     cohort = gets.chomp
+    puts "Age:"
+    age = gets.chomp
+    puts "City of birth:"
+    city = gets.chomp
+    puts "Preferred coding language:"
+    language = gets.chomp
+    puts "Gender"
+    gender = gets.chomp
     # Supplying a default value
     if cohort.empty?
       puts "You haven't entered a value. Please contact us."
@@ -16,7 +24,7 @@ def input_students
     end
     # Checking typos
     puts "You have entered:"
-    puts "Name: #{name}, Cohort: #{cohort}"
+    puts "Name: #{name}, Cohort: #{cohort}, Age: #{age}, City of birth: #{city}"
     puts "Is this correct? Y/N"
     response = gets.chomp.downcase
     if response == "n"
@@ -24,9 +32,17 @@ def input_students
       name = gets.chomp
       puts "Please re-enter cohort:"
       cohort = gets.chomp
+      puts "Please re-enter age:"
+      age = gets.chomp
+      puts "Please re-enter city of birth:"
+      city = gets.chomp
+      puts "Please re-enter your preferred coding language:"
+      language = gets.chomp
+      puts "Please re-enter gender:"
+      gender = gets.chomp
     end
     # add the student hash to the array
-    students << {name: name, cohort: cohort, age: :age, city_of_birth: :city, language_of_choice: :language, gender: :gender}
+    students << {name: name, cohort: cohort, age: age, city_of_birth: city, language_of_choice: language, gender: gender}
     if students.count == 1
       puts "Overall we have 1 great student."
     else
@@ -44,6 +60,33 @@ def input_students
   end
   students
 end
+
+def interactive_menu
+  students =[]
+  puts "Welcome to student input. Choose an action from the menu below:"
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input students"
+    puts "2. Show students"
+    puts "9. Exit"
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit  # this will cause the program to terminate
+    else
+      puts "I did not understand that. Please try again."
+    end
+  end
+end
+
 
 def print_header
   puts "The students of Villains Academy".center(80)
@@ -66,6 +109,7 @@ def print(students)
     puts "#{counter+1}. #{students[counter][:name]}, (#{students[counter][:cohort]} cohort), #{students[counter][:age]}, #{students[counter][:city_of_birth]}, #{students[counter][:language_of_choice]}, #{students[counter][:gender]}"
     counter += 1
   end
+  puts ("-" * 80).center(80)
 end
 
 # Sorting students by cohort month
@@ -84,14 +128,11 @@ end
 def print_footer(names)
   puts ("-" * 80).center(80)
   if names.count == 1
-    puts "Overall we have 1 great student."
+    puts "We have 1 amazing student."
   else
     puts "Overall we have #{names.count} great students."
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_cohort(students)
-print_footer(students)
+interactive_menu
+# print_cohort(students)
